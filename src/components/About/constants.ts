ef1d8139cb63fb5e32bb35fa088902fdb41ea0de
Serhii Yunnikov <yunnikov@icloud.com>
2024-11-04 18:49:05 +0100

    edited text about

--- src/components/About/constants.ts
+++ src/components/About/constants.ts
@@ -1,11 +1,12 @@
 export const text = "Hi, I'm Serhii, a Web Developer \
-with over 2 year of hands-on experience. \
-I specialize in React, React libraries, \
+with over 3 years of hands-on experience. \
+I specialize in React JS, React JS libraries, Next JS \
 Typescript, JavaScript, HTML, and CSS. \
-I'm well-versed in SASS, UI styling libraries, the BEM methodology. \
+I'm well-versed in SASS, UI styling libraries \
 My educational background includes a degree from the Faculty of \
 Applied Mathematics, I have an Intermediate - B2 level in English. \
-My journey into Web Development began 3 years ago when a colleague \
+Additionally, I'm actively learning German. \
+My journey into Web Development began 4 years ago when a colleague \
 inspired me to pursue this exciting career. \
 My adaptability shines through my ability to conquer new challenges, \
 balancing my studies with my passion for sports. As a coach, \
