aecf588b62b01b7f450001bcb622564c5128a8c5
Serhii Yunnikov <yunnikov@icloud.com>
2024-11-05 20:05:09 +0100

    added homepage in packaje.json

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio",
+  base: "/portfolio/",
   build: {
     assetsInlineLimit: 0,
   },
