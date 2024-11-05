91b7794f1aeb4a2b961f372bf73eb79cf5064738
Serhii Yunnikov <yunnikov@icloud.com>
2024-11-05 21:08:38 +0100

    fixed assets path images

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// });
-// vite.config.js or vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -14,18 +6,18 @@ export default defineConfig({
   base: "/portfolio/",
   build: {
     assetsInlineLimit: 0,
-  },
-  resolve: {
-    alias: {
-      // You can use an alias for your assets folder if needed
-      '@assets': '/src/assets',
-    },
-  },
-  // Add an image rule to handle SVG files
-  rules: [
-    {
-      test: /\.(svg)$/,
-      type: 'asset/resource',
-    },
-  ],
+  }
+  // resolve: {
+  //   alias: {
+  //     // You can use an alias for your assets folder if needed
+  //     '@assets': '/src/assets',
+  //   },
+  // },
+  // // Add an image rule to handle SVG files
+  // rules: [
+  //   {
+  //     test: /\.(svg)$/,
+  //     type: 'asset/resource',
+  //   },
+  // ],
 });
